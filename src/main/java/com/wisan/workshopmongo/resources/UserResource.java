80131e67752b3e0be337fc551a728a9f3c3711fd
Wisan <wisansilva3@gmail.com>
2023-07-03 18:25:31 -0300

    insert method with post

--- src/main/java/com/wisan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wisan/workshopmongo/resources/UserResource.java
@@ -1,15 +1,18 @@
 package com.wisan.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wisan.workshopmongo.domain.User;
 import com.wisan.workshopmongo.dto.UserDTO;
@@ -33,9 +36,16 @@ public class UserResource {
 	};
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = userService.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO objDTO) {
+		User obj = userService.fromDTO(objDTO);
+		obj = userService.insert(obj);
+	    URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	    return ResponseEntity.created(uri).build();
+	    }
 }
--- src/main/java/com/wisan/workshopmongo/service/UserService.java
+++ src/main/java/com/wisan/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wisan.workshopmongo.domain.User;
+import com.wisan.workshopmongo.dto.UserDTO;
 import com.wisan.workshopmongo.exception.ObjectNotFoundException;
 import com.wisan.workshopmongo.repository.UserRepository;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Not found"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
