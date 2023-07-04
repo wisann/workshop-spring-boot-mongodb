1b498a52ab9c2f9b3c438499607bdb9a406f47cc
Wisan <wisansilva3@gmail.com>
2023-07-04 18:46:05 -0300

    findByPost

--- src/main/java/com/wisan/workshopmongo/resources/UserResource.java
+++ src/main/java/com/wisan/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.wisan.workshopmongo.domain.Post;
 import com.wisan.workshopmongo.domain.User;
 import com.wisan.workshopmongo.dto.UserDTO;
 import com.wisan.workshopmongo.service.UserService;
@@ -63,6 +64,12 @@ public class UserResource {
 		obj.setId(id);
 		userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
